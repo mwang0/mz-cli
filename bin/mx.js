907feafa925114e6b051e9e265f1b92ae2ca29f6
NULL <xttnyw@gmail.com>
2019-07-31 21:58:08 +0800

    fix: 创建项目名为空出现异常
    #1

--- bin/mx.js
+++ bin/mx.js
@@ -1,22 +1,39 @@
 #!/usr/bin/env node
 
-const chalk = require('chalk')
-const semver = require('semver')
+const { enhanceErrorMessages } = require('../lib/utils')
 const program = require('commander')
+const chalk = require('chalk')
 
-program.version(require('../package.json').version).usage('<command> [options]')
+program
+  .version(require('../package.json').version)
+  .usage('<command> [options]')
 
 program
   .command('create <project-name>')
   .description('创建一个新项目')
   .action((name, cmd) => {
-    require('../lib/create')(name, {}).catch(ex => {
+    require('../lib/create')(name, {}).catch((ex) => {
       console.log(ex)
       process.exit(1)
     })
   })
 
-program.commands.forEach(c => c.on('--help', () => console.log()))
+program.commands.forEach((c) => c.on('--help', () => console.log()))
+
+enhanceErrorMessages('missingArgument', (argName) => {
+  return `Missing required argument ${chalk.yellow(`<${argName}>`)}.`
+})
+
+enhanceErrorMessages('unknownOption', (optionName) => {
+  return `Unknown option ${chalk.yellow(optionName)}.`
+})
+
+enhanceErrorMessages('optionMissingArgument', (option, flag) => {
+  return (
+    `Missing required argument for option ${chalk.yellow(option.flags)}` +
+    (flag ? `, got ${chalk.yellow(flag)}` : ``)
+  )
+})
 
 program.parse(process.argv)
 
